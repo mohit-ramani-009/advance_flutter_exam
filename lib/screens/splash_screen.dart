9fe3c2106677589890d9bc08b0b6e464059f651a
mohit_ramani_009 <158850519+mohit-ramani-009@users.noreply.github.com>
2024-12-04 11:56:57 +0530

    upload

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
     Future.delayed(Duration(seconds: 3), () {
       Navigator.pushReplacementNamed(context, '/');
     });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
-      body: Center(child: Text('All Countries',style: TextStyle(color: Colors.white,fontSize: 24,fontWeight: FontWeight.bold),)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 150),
+            Image.asset(
+              'assets/images/1200x630wa.png',
+              width: 200,
+              height: 150,
+            ),
+            SizedBox(height: 200),
+
+            Text(
+              'Country Encyclopedia App',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
+                shadows: [
+                  Shadow(
+                    blurRadius: 10.0,
+                    color: Colors.black.withOpacity(0.5),
+                    offset: Offset(2.0, 2.0),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
